e61b8838c4885138b652e880841d420481c88c3e
Phạm Văn Nam <nam.pv1805@gmail.com>
2023-02-20 10:11:50 +0700

    Add circuits

--- build/compute_witness.sh
+++ build/compute_witness.sh
@@ -1,4 +1,4 @@
 cd circuits/privDAO/privDAO_cpp
 make
-./privDAO ../../../input/input.json ../../../witness/witness.wtns
 cd -
+./circuits/privDAO/privDAO_cpp/privDAO ./input/input.json ./witness/witness.wtns
--- build/trusted_setup.sh
+++ build/trusted_setup.sh
@@ -1,8 +1,9 @@
 export resource_path="./circuits/resource"
 export r1cs_path="./circuits/privDAO/privDAO.r1cs"
-snarkjs powersoftau new bn128 18 $resource_path/pot18_0000.ptau -v
-snarkjs powersoftau contribute $resource_path/pot18_0000.ptau $resource_path/pot18_0001.ptau --name="First contribution" -v
-snarkjs powersoftau prepare phase2 $resource_path/pot18_0001.ptau $resource_path/pot18_final.ptau -v
-snarkjs groth16 setup $r1cs_path $resource_path/pot18_final.ptau $resource_path/privDAO_0000.zkey
+# snarkjs powersoftau new bn128 18 $resource_path/pot18_0000.ptau -v
+# snarkjs powersoftau contribute $resource_path/pot18_0000.ptau $resource_path/pot18_0001.ptau --name="First contribution" -v
+# snarkjs powersoftau prepare phase2 $resource_path/pot18_0001.ptau $resource_path/pot18_final.ptau -v
+# snarkjs groth16 setup $r1cs_path $resource_path/pot18_final.ptau $resource_path/privDAO_0000.zkey
+snarkjs groth16 setup $r1cs_path $resource_path/powersOfTau28_hez_final_18.ptau $resource_path/privDAO_0000.zkey
 snarkjs zkey contribute $resource_path/privDAO_0000.zkey $resource_path/privDAO_0001.zkey --name="1st Contributor Name" -v
 snarkjs zkey export verificationkey $resource_path/privDAO_0001.zkey $resource_path/verification_key.json
