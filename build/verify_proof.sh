bd878dc101d4390056d766e5799b6a3b5eaf272c
Phạm Văn Nam <nam.pv1805@gmail.com>
2023-02-22 10:10:15 +0700

    test

--- build/verify_proof.sh
+++ build/verify_proof.sh
@@ -1,4 +1,4 @@
-export $verification_key="./circuits/resource/verification_key.json"
-export $public="./circuits/resource/output/public.json"
-export $proof ="./circuits/resource/output/proof.json"
+export verification_key="./circuits/resource/verification_key.json"
+export public="./circuits/resource/output/public.json"
+export proof="./circuits/resource/output/proof.json"
 snarkjs groth16 verify $verification_key $public $proof
